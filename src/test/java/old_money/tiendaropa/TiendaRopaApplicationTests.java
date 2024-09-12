3509d8a4282b47448ae49c83162f65f153360812
Sebastian-M-C <sebastianmojica215@gmail.com>
2024-09-12 16:27:05 -0400

    Primera version del proyecto

---
+++ src/test/java/old_money/tiendaropa/TiendaRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package old_money.tiendaropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRopaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
